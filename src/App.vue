908e62d2af595511c8da1a9467b8f34927527167
DIVYANSH TRIPATHI <divyanshtripathi1998@gmail.com>
2019-07-28 01:39:44 +0530

    Gradients decided

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div id="app"></div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ImagePanel from "@/components/ImagePanel.vue";
+import Slots from "@/components/Slots.vue";
+import Word from "@/components/Word.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    ImagePanel,
+    Slots,
+    Word
   }
-}
+};
 </script>
 
 <style lang="scss">
+@import url("./variables.scss");
+body {
+  margin: 0;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  max-width: 500px;
+  border: 1px solid red;
+  margin: 0 auto;
+  height: 100%;
+  border-radius: 10%;
 }
 </style>
